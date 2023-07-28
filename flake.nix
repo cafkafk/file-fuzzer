521e1316df91807de91a658d0c064b8ce6db85b7
Christina Sørensen <christina@cafkafk.com>
2023-07-28 09:09:38 +0200

    feat: flakification
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  description = "A file fuzzer with fortune";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixos-23.05";
+
+  outputs = { self, nixpkgs }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+
+    in
+
+    {
+            # A Nixpkgs overlay.
+      overlay = final: prev: {
+
+        sfile = with final; stdenv.mkDerivation rec {
+          pname = "sfile";
+          inherit version;
+
+          src = ./.;
+
+          nativeBuildInputs = [ autoreconfHook ];
+        };
+
+      };
+
+
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        {
+          inherit (nixpkgsFor.${system}) sfile;
+        });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.sfile);
+
+      # Tests run by 'nix flake check' and by Hydra.
+      checks = forAllSystems
+        (system:
+          with nixpkgsFor.${system};
+
+          {
+            inherit (self.packages.${system}) sfile;
+
+            # Additional tests, if applicable.
+            test = stdenv.mkDerivation {
+              pname = "sfile-test";
+              inherit version;
+
+              buildInputs = [ sfile ];
+
+              dontUnpack = true;
+
+              buildPhase = ''
+                echo 'running some integration tests' | tee test-file.txt
+                [[ $(sfile test-file.txt) = 'test-file.txt: ASCII text' ]]
+              '';
+
+              installPhase = "mkdir -p $out";
+            };
+          }
+
+        );
+
+    };
+}
